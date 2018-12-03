9d9df2b419af1a72a982797b0ca6c876232071f7
Kody <kihnat-formfast@users.noreply.github.com>
2018-12-02 20:31:27 -0600

    Refactoring for Brute Force availability

--- RobertHalfProblem3/Program.cs
+++ RobertHalfProblem3/Program.cs
@@ -8,12 +8,16 @@ namespace RobertHalfProblem3
 {
     class Program
     {
-        //
-        private static List<List<string>> Result
-        {
-            get;
-            set;
-        }
+        /*
+         My Sudoku skills are quite limited.  The obvious approach is to start brute forcing numbers in and then make it check the solution.
+         This seems really inelegant and computationally expensive. 
+         I made the program to follow the steps that I follow when solving a Sudoku, and then make a series of reasonably educated guesses and checking those guesses.
+         It's still brute forcing it, but it should provide a solution faster then straight brute force.
+         Given the time constraints of this challenge I am not going to learn how to be a master of sudoku techniques and then program them.
+         I realize that the brute force is a complete solution on its own, but I think this shows off greater coding skill.
+         I'm sure it is possible to program enough Sudoku techniques to not need any brute force, but I am not 
+
+        */
 
         private List<SquareOptions> OptionsList
         {
@@ -22,36 +26,68 @@ namespace RobertHalfProblem3
 
         static void Main(string[] args)
         {
-            SolvePuzzle(Test4);
+            SolvePuzzle(Test5);
         }
 
         private static void SolvePuzzle(List<List<string>> input)
         {
-            Result = input;
             var optionsList = ConvertToSquareOptions(input);
+            var puzzle = SolvePuzzleLogically(optionsList);
+
+
+            var unsolvedSquares = puzzle.Where(x => x.Value == ".").Count();
+            if (unsolvedSquares != 0)
+            {
+                puzzle = SolvePuzzleWithBruteForce(puzzle);
+
+                unsolvedSquares = puzzle.Where(x => x.Value == ".").Count();
+                if (unsolvedSquares != 0)
+                {
+                    Console.WriteLine("I failed to solve the puzzle completely.  This is what I have.");
+                    Console.WriteLine("My programming is insufficient or a solution is not possible.");
+                }
+
+            }
+            if (CheckBoardState(puzzle))
+            {
+                ShowResult(puzzle);
+            }
+            else
+            {
+                Console.WriteLine("Oops, something went wrong");
+            }
+
+        }
+
+        private static List<SquareOptions> SolvePuzzleLogically(List<SquareOptions> optionsList)
+        {
+            var ret = optionsList;
             bool progressBeingMade = true;
 
             while (progressBeingMade)
             {
                 progressBeingMade = false;
 
-                var solvedCells = optionsList.Where(x => x.Value != ".").ToList();
+                var solvedCells = ret.Where(x => x.Value != ".").ToList();
 
                 //Update cell options based on what is in their row/column/box
                 foreach (var cell in solvedCells)
                 {
                     string solvednumber = cell.Value;
-                    var matchingX = optionsList.Where(x => x.XCoordinate == cell.XCoordinate && x.YCoordinate != cell.YCoordinate);
-                    var matchingY = optionsList.Where(x => x.YCoordinate == cell.YCoordinate && x.XCoordinate != cell.XCoordinate);
-                    var matchingZ = optionsList.Where(x => x.BoxNumber == cell.BoxNumber && (x.YCoordinate != cell.YCoordinate || x.XCoordinate != cell.XCoordinate));
+
+                    var matchingX = ret.Where(x => x.XCoordinate == cell.XCoordinate && x.YCoordinate != cell.YCoordinate);
                     foreach (var xMatchedCell in matchingX)
                     {
                         xMatchedCell.Options.Remove(solvednumber);
                     }
+
+                    var matchingY = ret.Where(x => x.YCoordinate == cell.YCoordinate && x.XCoordinate != cell.XCoordinate);
                     foreach (var yMatchedCell in matchingY)
                     {
                         yMatchedCell.Options.Remove(solvednumber);
                     }
+
+                    var matchingZ = ret.Where(x => x.BoxNumber == cell.BoxNumber && (x.YCoordinate != cell.YCoordinate || x.XCoordinate != cell.XCoordinate));
                     foreach (var boxMatchedCell in matchingZ)
                     {
                         boxMatchedCell.Options.Remove(solvednumber);
@@ -61,72 +97,115 @@ namespace RobertHalfProblem3
                 //Update Cell Options based on each row needing something of every value
                 for (int i = 0; i < 9; i++)
                 {
-                    var rowCells = optionsList.Where(x => x.XCoordinate == i);
-                    for (int j = 1; j <= 9; j++)
-                    {
-                        var searchValue = j.ToString();
-                        if (rowCells.Where(x => x.Value == searchValue).Count() == 0)
-                        {
-                            var possibleCells = rowCells.Where(x => x.Options.Contains(searchValue));
-                            if (possibleCells.Count() == 1)
-                            {
-                                var value = new List<string> { searchValue };
-                                possibleCells.First().Options = value;
-                            }
-                        }
-                    }
+                    var rowCells = ret.Where(x => x.XCoordinate == i).ToList();
+                    SearchForOnlyOptionInGroup(rowCells);
                 }
 
                 //Update Cell Options based on each column needing something of every value
                 for (int i = 0; i < 9; i++)
                 {
-                    var columnCells = optionsList.Where(x => x.YCoordinate == i);
-                    for (int j = 1; j <= 9; j++)
-                    {
-                        var searchValue = j.ToString();
-                        if (columnCells.Where(x => x.Value == searchValue).Count() == 0)
-                        {
-                            var possibleCells = columnCells.Where(x => x.Options.Contains(searchValue));
-                            if (possibleCells.Count() == 1)
-                            {
-                                var value = new List<string> { searchValue };
-                                possibleCells.First().Options = value;
-                            }
-                        }
-                    }
+                    var columnCells = ret.Where(x => x.YCoordinate == i).ToList();
+                    SearchForOnlyOptionInGroup(columnCells);
                 }
 
                 //Update Cell Options based on each box needing something of every value
                 for (int i = 1; i <= 9; i++)
                 {
-                    var boxCells = optionsList.Where(x => x.BoxNumber == i);
-                    for (int j = 1; j <= 9; j++)
-                    {
-                        var searchValue = j.ToString();
-                        if (boxCells.Where(x => x.Value == searchValue).Count() == 0)
-                        {
-                            var possibleCells = boxCells.Where(x => x.Options.Contains(searchValue));
-                            if (possibleCells.Count() == 1)
-                            {
-                                var value = new List<string> { searchValue };
-                                possibleCells.First().Options = value;
-                            }
-                        }
-                    }
+                    var boxCells = ret.Where(x => x.BoxNumber == i).ToList();
+                    SearchForOnlyOptionInGroup(boxCells);
                 }
 
                 //Fill in the solved cells
-                var solvableCells = optionsList.Where(x => x.Value == "." && x.Options.Count() == 1).ToList();
+                var solvableCells = ret.Where(x => x.Value == "." && x.Options.Count() == 1).ToList();
                 foreach (var cell in solvableCells)
                 {
                     var cellValue = cell.Options.First();
                     cell.Value = cellValue;
-                    Result[cell.XCoordinate][cell.YCoordinate] = cellValue;
                     progressBeingMade = true;
                 }
             }
+            return ret;
 
-            ShowResult();
+        }
+
+        private static List<SquareOptions> SolvePuzzleWithBruteForce(List<SquareOptions> puzzle)
+        {
+            //Make a list of guesses.  That's empty cells paired with a valid option from their options list
+
+            //Pick a guess.  Make that guess.
+
+            //Run that guess through the logic.
+
+            //Either it will fill in the bord completely or it won't. 
+
+            //Check the answer. 
+
+            //If it is incomplete, hop down another level and make another guess
+            bool isCorrect = CheckBoardState(puzzle);
+            return puzzle;
+        }
+
+        private static bool CheckBoardState(List<SquareOptions> puzzle)
+        {
+            var ret = true;
+            //Check Rows
+            for (int i = 0; i < 9; i++)
+            {
+                var rowCells = puzzle.Where(x => x.XCoordinate == i).ToList();
+                ret = ret && CheckSubsetForDuplicates(rowCells);
+            }
+
+            //Check Columns
+            for (int i = 0; i < 9; i++)
+            {
+                var columnCells = puzzle.Where(x => x.YCoordinate == i).ToList();
+                ret = ret && CheckSubsetForDuplicates(columnCells);
+            }
+
+            //Check Boxes
+            for (int i = 0; i < 9; i++)
+            {
+                var boxCells = puzzle.Where(x => x.BoxNumber == i).ToList();
+                ret = ret && CheckSubsetForDuplicates(boxCells);
+            }
+
+            //Check Options
+            var unfillableCells = puzzle.Where(x => x.Options.Count() == 0).Count();
+            ret = ret && unfillableCells == 0;
+
+            return ret;
+        }
+
+        private static bool CheckSubsetForDuplicates(List<SquareOptions> set)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                var searchValue = j.ToString();
+                var results = set.Where(x => x.Value == searchValue);
+                if (results.Count() > 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void SearchForOnlyOptionInGroup(List<SquareOptions> cellGroup)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                var searchValue = j.ToString();
+                if (cellGroup.Where(x => x.Value == searchValue).Count() == 0)
+                {
+                    var possibleCells = cellGroup.Where(x => x.Options.Contains(searchValue));
+                    if (possibleCells.Count() == 1)
+                    {
+                        var value = new List<string> { searchValue };
+                        possibleCells.First().Options = value;
+                    }
+                }
+            }
         }
 
         private static List<SquareOptions> ConvertToSquareOptions(List<List<string>> input)
@@ -188,13 +267,14 @@ namespace RobertHalfProblem3
             }
         }
 
-        public static void ShowResult()
+        public static void ShowResult(List<SquareOptions> result)
         {
             for (int x = 0; x < 9; x++)
             {
                 for (int y = 0; y < 9; y++)
                 {
-                    Console.Write((Result.ElementAt(x)).ElementAt(y) + "\t");
+                    var element = result.Where(i => i.XCoordinate == x && i.YCoordinate == y).First().Value;
+                    Console.Write(element + "\t");
                 }
                 Console.WriteLine();
             }
@@ -205,7 +285,7 @@ namespace RobertHalfProblem3
         {
             get
             {
-                //The input type is string to allow for Hex Values
+                //The input type is string to allow for the placeholder
                 List<List<string>> input = new List<List<string>>(new List<string>[9] {
                     new List<string>(new string[9] { "8", ".", ".",       "9", "3", ".",      ".", ".", "2" }),
                     new List<string>(new string[9] {".", ".", "9",        ".", ".", ".",      ".", "4", "."}),
@@ -226,7 +306,7 @@ namespace RobertHalfProblem3
         {
             get
             {
-                //The input type is string to allow for Hex Values
+                //Easy Level Puzzle
                 List<List<string>> input = new List<List<string>>(new List<string>[9] {
                     new List<string>(new string[9] {"5", ".", ".",        "6", ".", ".",      "2", ".", "." }),
                     new List<string>(new string[9] {".", "8", ".",        ".", ".", "7",      ".", "1", "3"}),
@@ -247,7 +327,7 @@ namespace RobertHalfProblem3
         {
             get
             {
-                //The input type is string to allow for Hex Values
+                //Medium Level Puzzle
                 List<List<string>> input = new List<List<string>>(new List<string>[9] {
                     new List<string>(new string[9] {"3", "1", "6",        "4", "2", ".",      ".", ".", "7" }),
                     new List<string>(new string[9] {".", "2", ".",        "1", ".", ".",      "9", ".", "."}),
@@ -268,7 +348,7 @@ namespace RobertHalfProblem3
         {
             get
             {
-                //The input type is string to allow for Hex Values
+                //Hard Level Puzzle
                 List<List<string>> input = new List<List<string>>(new List<string>[9] {
                     new List<string>(new string[9] {".", ".", ".",        ".", ".", ".",      ".", "3", "8" }),
                     new List<string>(new string[9] {"2", ".", ".",        ".", ".", ".",      ".", ".", "."}),
@@ -279,8 +359,29 @@ namespace RobertHalfProblem3
                     new List<string>(new string[9] {"7", ".", ".",        ".", ".", ".",      "9", ".", "."}),
 
                     new List<string>(new string[9] {".", "5", ".",        ".", ".", ".",      ".", ".", "."}),
-                    new List<string>(new string[9] {".", ".", ".",        ".", ".", "5",      "3.", ".", "."}),
+                    new List<string>(new string[9] {".", ".", ".",        ".", ".", "5",      "3", ".", "."}),
                     new List<string>(new string[9] {"9", ".", ".",        ".", ".", "1",      "6", ".", "2"})});
+                return input;
+            }
+        }
+
+        private static List<List<string>> Test5
+        {
+            get
+            {
+                //This is left blank to make it easier to insert new puzzles
+                List<List<string>> input = new List<List<string>>(new List<string>[9] {
+                    new List<string>(new string[9] {".", ".", ".",        ".", ".", ".",      "6", "8", "." }),
+                    new List<string>(new string[9] {".", ".", ".",        ".", "7", "3",      ".", ".", "9"}),
+                    new List<string>(new string[9] {"3", ".", "9",        ".", ".", ".",      ".", "4", "5"}),
+
+                    new List<string>(new string[9] {"4", "9", ".",        ".", ".", ".",      ".", ".", "."}),
+                    new List<string>(new string[9] {"8", ".", "3",        ".", "5", ".",      "9", ".", "2"}),
+                    new List<string>(new string[9] {".", ".", ".",        ".", ".", ".",      ".", "3", "6"}),
+
+                    new List<string>(new string[9] {"9", "6", ".",        ".", ".", ".",      "3", ".", "8"}),
+                    new List<string>(new string[9] {"7", ".", ".",        "6", "8", ".",      ".", ".", "."}),
+                    new List<string>(new string[9] {".", "2", "8",        ".", ".", ".",      ".", ".", "."})});
                 return input;
             }
         }
@@ -289,7 +390,7 @@ namespace RobertHalfProblem3
         {
             get
             {
-                //The input type is string to allow for Hex Values
+                //This is left blank to make it easier to insert new puzzles
                 List<List<string>> input = new List<List<string>>(new List<string>[9] {
                     new List<string>(new string[9] {".", ".", ".",        ".", ".", ".",      ".", ".", "." }),
                     new List<string>(new string[9] {".", ".", ".",        ".", ".", ".",      ".", ".", "."}),
